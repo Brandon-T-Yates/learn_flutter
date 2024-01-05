ba22b2fcfecb8ffd288f1b70ac69b406507d150c
Brandon Yates <BTYates@student.fullsail.edu>
2024-01-04 19:29:25 -0500

    Learned buttons, Icons, Color Swatch, Nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,41 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Gets rid of the banner in top right corner
+      theme: ThemeData(
+        primarySwatch: Colors.blue // Sets color theme for app
+      ),
+      home: const RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( // Nav bar
+        title: const Text('Flutter'), // Adds title to the nav bar
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          debugPrint('Floating Action Button'); // Prints to the terminal
+        },
+        child: Icon(Icons.add), // Adds the icnon within the button
+      ),
+    );
+  }
+}
