c8a22575245e218bced8e5fe9692ab05c3389d95
Brandon Yates <BTYates@student.fullsail.edu>
2024-01-04 23:14:23 -0500

    created profile page with list view and items

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learn_flutter/home_page.dart';
+import 'package:learn_flutter/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,13 +30,17 @@ class RootPage extends StatefulWidget {
 
 class _RootPageState extends State<RootPage> {
   int currentPage = 0; // created a variable
+  List<Widget> pages = const [ // Creates a list of pages
+    HomePage(), // List Item
+    ProfilePage(), // List Item
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar( // Nav bar
         title: const Text('Flutter'), // Adds title to the nav bar
       ),
-      body: const HomePage(),
+      body: pages[currentPage],
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           debugPrint('Floating Action Button'); // Prints to the terminal
---
+++ lib/profile_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+int itemCount = 20; // Created variable
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: itemCount, // Called variable and told it how many to list
+      itemBuilder: (BuildContext context, int index) { // Builds the list
+        return ListTile( // Returns list.....
+          title: Text('Item ${(index + 1)}'), // Items text and increases by 1!
+          leading: const Icon(Icons.person),
+          trailing: const Icon(Icons.select_all),
+          onTap: () {
+            debugPrint('Item ${(index + 1)} selected');
+          },
+        );
+      },
+    );
+  }
+}
